1cfedf70f5443620fe65d68ee5b26af93e78a39f
YaroslavPavliuk <pyarchik@gmail.com>
2023-05-25 21:27:04 +0300

    добавил токен и понимание голоса и текста

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
-import {Telegraf } from 'telegraf'
+import { Telegraf } from 'telegraf'
+import { message } from 'telegraf/filters'
+import config from 'config'
 
-const bot = new Telegraf('')
+const bot = new Telegraf(config.get('TELEGRAM_TOKEN'))
 
-bot.launcher()
+bot.on(message('text'), async ctx =>{
+    await ctx.reply(JSON.stringify(ctx.message, null, 2))
+})
+
+bot.on(message('voice'), async ctx =>{
+    await ctx.reply(JSON.stringify(ctx.message.voice, null, 2))
+})
+
+bot.command('start', async (ctx) => {
+    await ctx.reply(JSON.stringify(ctx.message, null, 2))
+})
+
+
+bot.launch()
 
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
